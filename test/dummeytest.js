7ff924fe9f2af40e1dc8a615f8daa4e848f054a4
eliran zach <thehobbit85@gmail.com>
2015-06-18 16:03:57 +0300

    fixed test

--- test/dummeytest.js
+++ test/dummeytest.js
@@ -1,9 +1,11 @@
 var assert = require("assert")
+var app = require(__dirname + '/../server')
 describe('Array', function(){
   describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
+    it('should return -1 when the value is not present', function (done){
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+      done()
     })
   })
 })
